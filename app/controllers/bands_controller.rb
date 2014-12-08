6fd4fc5b2aa1100273c15ce35e61685ba7e43c0c
stbarrientos <stbarrientos@gmail.com>
2014-12-07 21:43:21 -0500

    everything but controllers

---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,20 @@
+class BandsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+
+end
