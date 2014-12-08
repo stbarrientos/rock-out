b8084aa1b17e14f2d481f64310770bcc84876c11
stbarrientos <stbarrientos@gmail.com>
2014-12-07 22:02:34 -0500

    hello world page is up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  devise_scope :user do
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
+#  devise_scope :user do
+#    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+#  end
+
+  root "bands#index"
+
+  resources :bands
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
